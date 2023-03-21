8e73d37198abf4168f381154e405090f2fa6f384
mounyaze <mounya.zekraoui@enim.ac.ma>
2023-03-21 09:41:46 +0000

    Ajoutez des fichiers projet.

---
+++ MicrosoftTutoApi/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MicrosoftTutoApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    namespace TodoApi.Models
+    {
+        public class TodoContext : DbContext
+        {
+            public TodoContext(DbContextOptions<TodoContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; }
+        }
+    }
+}
